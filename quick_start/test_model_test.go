c6a129e18c2246aac0595df74373198eafc86a81
zouchangfu <zcf13543354971@163.com>
2022-07-30 20:19:29 +0800

    refactor: reactor quick_start

--- quick_start/test_model_test.go
+++ quick_start/test_model_test.go
@@ -3,18 +3,45 @@ package quick_start
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/acmestack/gobatis-example/transaction/db"
+	"github.com/acmestack/gobatis"
+	"github.com/acmestack/gobatis/datasource"
+	"github.com/acmestack/gobatis/factory"
+	_ "github.com/go-sql-driver/mysql"
 	"testing"
 	"time"
 )
 
+var sessionManager *gobatis.SessionManager
+
+func init() {
+	err := gobatis.RegisterMapperFile("./xml/test_table_mapper.xml")
+	if err != nil {
+		fmt.Println("parse xml is error", err.Error())
+	}
+	sessionManager = gobatis.NewSessionManager(connect())
+}
+
+func connect() factory.Factory {
+	return gobatis.NewFactory(
+		gobatis.SetMaxConn(100),
+		gobatis.SetMaxIdleConn(50),
+		gobatis.SetDataSource(&datasource.MysqlDataSource{
+			Host:     "localhost", // 数据库IP
+			Port:     3306,        // 数据库端口
+			DBName:   "test",      // 数据库名
+			Username: "root",      // 数据库用户名
+			Password: "123456",    // 数据库密码
+			Charset:  "utf8",      // 编码格式
+		}))
+}
+
 func TestTestTable_Insert(t *testing.T) {
 	testTable := &TestTable{
 		CreateTime: time.Now(),
-		Username:   "user",
+		Username:   "user1",
 		Password:   "123456",
 	}
-	result, id, err := testTable.Insert(db.SessionManager.NewSession())
+	result, id, err := testTable.Insert(sessionManager.NewSession())
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -24,7 +51,7 @@ func TestTestTable_Insert(t *testing.T) {
 
 func TestTestTable_Select(t *testing.T) {
 	table := &TestTable{}
-	tables, err := table.Select(db.SessionManager.NewSession())
+	tables, err := table.Select(sessionManager.NewSession())
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -34,7 +61,7 @@ func TestTestTable_Select(t *testing.T) {
 
 func TestTestTable_Count(t *testing.T) {
 	table := &TestTable{}
-	count, err := table.Count(db.SessionManager.NewSession())
+	count, err := table.Count(sessionManager.NewSession())
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -46,7 +73,7 @@ func TestTestTable_Update(t *testing.T) {
 		Id:       1,
 		Password: "654321",
 	}
-	result, err := table.Update(db.SessionManager.NewSession())
+	result, err := table.Update(sessionManager.NewSession())
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -57,7 +84,7 @@ func TestTestTable_Delete(t *testing.T) {
 	table := &TestTable{
 		Username: "user1",
 	}
-	result, err := table.Delete(db.SessionManager.NewSession())
+	result, err := table.Delete(sessionManager.NewSession())
 	if err != nil {
 		fmt.Println(err)
 	}
